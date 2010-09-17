7419327321c36b4ae5f8b57aa56e7d629d349d36
LeTama <letama@free.fr>
2010-09-17 12:08:43 +0200

    Disable console and enable boot splash screen

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1086,12 +1086,12 @@ static int msmfb_probe(struct platform_device *pdev)
 #ifdef CONFIG_FB_MSM_LOGO
 	if (!load_565rle_image(INIT_IMAGE_FILE)) {
 		/* Flip buffer */
-		msmfb->update_info.left = 0;
+	        msmfb->update_info.left = 0;
 		msmfb->update_info.top = 0;
-		msmfb->update_info.eright = info->var.xres;
-		msmfb->update_info.ebottom = info->var.yres;
-		msmfb_pan_update(info, 0, 0, fb->var.xres,
-				 fb->var.yres, 0, 1);
+		msmfb->update_info.eright = msmfb->xres;
+	        msmfb->update_info.ebottom = msmfb->yres;
+		msmfb_pan_update(msmfb->fb, 0, 0, msmfb->xres,
+				 msmfb->yres, 0, 1);
 	}
 #endif
 	/* Jay, 29/12/08' */
