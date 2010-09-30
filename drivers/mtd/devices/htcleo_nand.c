5aabf56c357d5bc0f4cd199bd2eb0900701691f3
rajkosto <admin@rajko.info>
2010-09-30 19:29:53 +0200

    nand driver oob layout fix

--- drivers/mtd/devices/htcleo_nand.c
+++ drivers/mtd/devices/htcleo_nand.c
@@ -162,7 +162,7 @@ static struct nand_ecclayout msm_nand_oob_64 =
 		0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  // 10, 11, 12, 13, // 14, 15, // ecc, oob, bbf (inaccessible)
 		16, 17, 18, 19, 20, 21, 22, 23, 24, 25, // 26, 27, 28, 29, // 30, 31, // ecc, oob, bbf (inaccessible)
 		32, 33, 34, 35, 36, 37, 38, 39, 40, 41, // 42, 43, 44, 45, // 46, 47, // ecc, oob, bbf (inaccessible)
-		46, 47, 48, 49, 50, 51, 52, 53, 54, 55, // 56, 57, 58, 59, // 60, 61, // ecc, oob, bbf (inaccessible)
+		48, 49, 50, 51, 52, 53, 54, 55, 56, 57, // 58, 59, 60, 61, // 62, 63, // ecc, oob, bbf (inaccessible)
 	},
 	.oobavail = 16,
 	.oobfree = 
@@ -170,7 +170,7 @@ static struct nand_ecclayout msm_nand_oob_64 =
 		{10, 4},
 		{26, 4},
 		{42, 4},
-		{56, 4},
+		{58, 4},
 	}
 };
 
