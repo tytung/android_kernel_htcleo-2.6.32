d8579fa1096f2db4c714b3328ac30b7a3e598650
tytung <tytung+git@gmail.com>
2012-01-07 20:47:05 +0800

    drivers: cpufreq: added smartass2 governor.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -347,6 +347,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_interactivex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass2)
 #endif
 
 
